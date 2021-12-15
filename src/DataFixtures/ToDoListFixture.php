5864e40b2743e7a26510e8d8a1ec177df71259f1
Mael Jamin <mael.jamin@gmail.com>
2021-12-15 12:35:03 +0100

    Fixtures User et TodoList + Faker

--- src/DataFixtures/ToDoListFixture.php
+++ src/DataFixtures/ToDoListFixture.php
@@ -2,16 +2,46 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Item;
+use App\Entity\ToDoList;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\DataFixtures\UserFixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class ToDoListFixture extends Fixture
+use Faker;
+
+class ToDoListFixture extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create("fr_FR");
 
+        $user = $manager->getRepository(User::class)->findBy(['email'=>'toto@gmail.com'])[0];
+        $todolist = new ToDoList();
+        $todolist
+            ->setName("TodoList Toto")
+            ->setOwner($user);
+
+        for ($i = 0; $i <= random_int(1,7); $i++){
+            $todolist_item = new Item();
+            $todolist_item
+                ->setName("item$i")
+                ->setContent($faker->sentence)
+                ->setCreatedAt(new \DateTimeImmutable())
+                ->setToDoList($todolist);
+            $manager->persist($todolist_item);
+            //$manager->flush();
+        }
+        $manager->persist($todolist);
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            UserFixture::class,
+        ];
     }
 }
--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -5,15 +5,30 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixture extends Fixture
 {
+    private $encoder;
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager): void
     {
         $user = new User();
-        // $product = new Product();
-        // $manager->persist($product);
 
+        $password = "123456789";
+        $password_encoded = $this->encoder->hashPassword($user,$password);
+
+        $user
+            ->setEmail("toto@gmail.com")
+            ->setPassword($password_encoded)
+            ->setFirstname("Toto")
+            ->setLastname("Tata")
+            ->setBirthdate(new \DateTime("01-01-1999"));
+        // $product = new Product();
+        $manager->persist($user);
         $manager->flush();
     }
 }
