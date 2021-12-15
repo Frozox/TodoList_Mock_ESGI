54cb1834592ffaae9e8d9afada08604ed4b8b698
Mael Jamin <mael.jamin@gmail.com>
2021-12-15 11:43:52 +0100

    Fixture User

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -4,11 +4,13 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
 
 class UserFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $user = new User();
         // $product = new Product();
         // $manager->persist($product);
 
